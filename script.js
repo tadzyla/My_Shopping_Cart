95257aef05fe24229ce9af63e9fd8ab366c2adb4
tadzyla <tadas.zvinklys@gmail.com>
2021-10-06 11:07:41 +0100

    adding product list

--- script.js
+++ script.js
@@ -1,7 +1,44 @@
-function add() {
+let productList = [{
+    id: 'A',
+    price: 50,
+    specialCount: 3,
+    specialPrice:130, 
+    count:0,
+    total:0
+},
+{
+    id: 'B',
+    price: 30,
+    specialCount: 2,
+    specialPrice:45, 
+    count:0,
+    total: 0
+},
+{
+    id: 'C',
+    price: 20,
+    specialCount: 0,
+    specialPrice:0,
+    count:0,
+    total: 0
+},
+{
+    id: 'D',
+    price: 15,
+    specialCount: 0,
+    specialPrice:0,
+    count:0,
+    total: 0
+}]
 
+function add() {
+    updateTotalPrice()
 }
 
 function remove() {
-    
+    updateTotalPrice()
+}
+
+function updateTotalPrice() {
+
 }
